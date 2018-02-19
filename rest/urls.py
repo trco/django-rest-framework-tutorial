f8991b4189fa6a8da10687c5d32e83f74ee931a7
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-02-19 22:32:16 +0100

    change snippets app to tutorial_1 app

--- rest/urls.py
+++ rest/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('', include('snippets.urls', namespace='snippets')),
+    path('tutorial_1/', include('tutorial_1.urls', namespace='tutorial_1')),
 ]
--- tutorial_1/urls.py
+++ tutorial_1/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'snippets'
+app_name = 'tutorial_1'
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<pk>', views.snippet_detail),
